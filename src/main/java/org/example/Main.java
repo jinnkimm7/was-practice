612f199c3c55d2506ec0ee96febc8674481299f6
Jin <jinnkimm7@gmail.com>
2024-06-13 15:29:46 +0900

    Apply .gitignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 // GET /calcuate?operand1=11&operate=*operand2=2=55
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new CustomWebApplicationServer(8080).start();
     }
 }
